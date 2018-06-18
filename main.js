b2a6766b365b1e6c350b298fb5fd00a43ee1e9ad
Niclas <niclas.victorsson@medieinstitutet.se>
2018-06-18 12:08:03 +0200

    class and constructor made for tracks and module for handlingtracks created with a method for checking localStorage

--- main.js
+++ main.js
@@ -29,21 +29,19 @@ const handleTracks = {
 
     checkLocalStorage: function() {
         //this is the first thing thats bound to happen when loading the site
-        //check if there are tracks saved in localStorage, if not create a track as an example
+        //check if there are tracks saved in localStorage, if not create the playlist and a track as an example
         const ultimatePlaylist = JSON.parse(localStorage.getItem('ultimatePlaylist'));
 
         if(ultimatePlaylist) {
             console.log("there is a playlist saved!!")
         } else {
             const ultimatePlaylist = []; //Define ultimatePlaylist that will collect all the tracks
-            var newTrack = new track("Artist", "Song name"); //Construct a new track
+            var newTrack = new track("Artist", "Song name"); //Construct a new example track
             ultimatePlaylist.push(newTrack) //Push the track to the ultimatePlaylist array
 
-            localStorage.setItem("ultimatePlaylist", JSON.stringify(ultimatePlaylist));
-            console.log(ultimatePlaylist);
+            localStorage.setItem("ultimatePlaylist", JSON.stringify(ultimatePlaylist)); //Save it to localStorage
         }
     },
-
 }
 
 //Launch localStorage-check
